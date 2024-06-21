987f57699baa37d2dfd4a13120bc2d281b229432
obafisayo <abimbolaobafisayo@gmail.com>
2024-06-21 12:51:15 +0100

    added  pulse box

--- src/component/reviews/Reviews.jsx
+++ src/component/reviews/Reviews.jsx
@@ -72,29 +72,60 @@ const ReviewForm = () => {
         navigate("/login")
     }
   };
-
+  const Pulsebox = () => {
+    return(
+      <div className="border border-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
+        <div className="animate-pulse flex space-x-4">
+          <div className="rounded-full bg-slate-700 h-10 w-10"></div>
+          <div className="flex-1 space-y-6 py-1">
+            <div className="h-2 bg-slate-700 rounded"></div>
+            <div className="space-y-3">
+              <div className="grid grid-cols-3 gap-4">
+                <div className="h-2 bg-slate-700 rounded col-span-2"></div>
+                <div className="h-2 bg-slate-700 rounded col-span-1"></div>
+              </div>
+              <div className="h-2 bg-slate-700 rounded"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  const [s, ss] = useState(false)
+  setTimeout(() => {
+    ss(true)
+  }, 3000)
   return (
     <div className="bg-gray-100 py-8 px-4 sm:px-6 lg:px-8">
         <div className="mt-8 max-w-2xl mx-auto">
-            <h2 className="text-3xl font-bold mb-6">Existing Reviews</h2>
+          <h2 className="text-3xl font-bold mb-6">Existing Reviews</h2>
+          {s? 
+            <>
             {reviews.map((review, index) => (
                 <div key={index} className="bg-white rounded-lg shadow-md p-6 mb-6">
-                    <div className="flex items-center mb-4">
-                        <FaStar className="text-yellow-500 mr-2" />
-                        <h3 className="text-lg font-bold">{review.landlord}</h3>
+                  <div className="flex items-center mb-4">
+                    <FaStar className="text-yellow-500 mr-2" />
+                    <h3 className="text-lg font-bold">{review.landlord}</h3>
+                  </div>
+                  <p className="mb-4">{review.environment}</p>
+                  <p className="mb-4">{review.amenities}</p>
+                  <div className="flex space-x-4">
+                    <div className="w-1/4">
+                      <img src={parsedImg[index]} alt={`img-${index}`} className="rounded-md" />
                     </div>
-                    <p className="mb-4">{review.environment}</p>
-                    <p className="mb-4">{review.amenities}</p>
-                    <div className="flex space-x-4">
-                        <div className="w-1/4">
-                            <img src={parsedImg[index]} alt={"img"} className="rounded-md" />
-                        </div>
-                        <div className="w-3/4">
-                            <video src={parsedvid[index]} controls className="rounded-md" />
-                        </div>
+                    <div className="w-3/4">
+                      <video src={parsedvid[index]} controls className="rounded-md" />
                     </div>
+                  </div>
                 </div>
             ))}
+            </>
+              : 
+            <>
+              <Pulsebox />
+              <p className="truncate w-[8rem] items-center">loading reviews please wait!!!</p>
+            </>
+          }
         </div>
         <div className="max-w-2xl mx-auto container order-2">
             <h2 className="text-3xl font-bold mb-6">Create a Review</h2>
